191fb0d7e35fa5f3860f162dcdf17a790fb5a3af
Rishabh Gupta <rg3334@columbia.edu>
2021-04-01 15:45:56 -0700

    fixed return code handling

--- test/test.c
+++ test/test.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <stdint.h>
+#include <sys/wait.h>
 
 #define __NR_get_pagetable_layout 436
 #define __NR_expose_page_table 437
@@ -76,14 +77,14 @@ struct temp {
 };
 
 
-int print_pgtbl_range(unsigned long start, unsigned long end)
+int get_pagetable_range(unsigned long start, unsigned long end)
 {
     struct pagetable_layout_info pgtbl_info;
     struct expose_pgtbl_args pgtbl_args;
     pid_t pid;
     int pgd_size, p4d_size, pud_size, pmd_size, pte_size, verbose, ret;
     unsigned long va_begin, va_end, current_va, *addr, *addr1, *addr2, *addr3, *addr4;
-    unsigned long *fake_pgd, *fake_p4ds, *fake_puds, *fake_pmds, *fake_ptes;
+    unsigned long *fake_pgd;
     unsigned long *f_pgd, *f_p4d, *f_pud, *f_pmd, f_pte;
     
     verbose = 1;
@@ -100,7 +101,7 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
     if (ret < 0) {
         fprintf(stderr, "Error : %s\n", strerror(errno));
         printf("Get Page Table Layout System Call Failed.\n");
-        exit(EXIT_FAILURE);
+        return ret;
     }
     
     pgd_size = PTR_PER_PXX * sizeof(unsigned long);
@@ -123,9 +124,9 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
    
     addr1 = mmap(NULL, p4d_size, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     if (addr1 == MAP_FAILED) {
-	    free(addr);
-	    fprintf(stderr, "Error : %s\n", strerror(errno));
-	    exit(EXIT_FAILURE);
+        free(addr);
+        fprintf(stderr, "Error : %s\n", strerror(errno));
+        return -1;
     }
      
     pgtbl_args.fake_p4ds = (unsigned long)addr1;
@@ -135,7 +136,7 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
     free(addr);
     munmap(addr1,p4d_size);
         fprintf(stderr, "Error : %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     pgtbl_args.fake_puds = (unsigned long)addr2;
@@ -145,7 +146,7 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
     munmap(addr1,p4d_size);
     munmap(addr2,pud_size);
         fprintf(stderr, "Error : %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return -1;
     }
  
     pgtbl_args.fake_pmds = (unsigned long)addr3;
@@ -156,7 +157,7 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
     munmap(addr2,pud_size);
     munmap(addr3,pmd_size);
         fprintf(stderr, "Error : %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     pgtbl_args.page_table_addr = (unsigned long)addr4;
@@ -164,34 +165,37 @@ int print_pgtbl_range(unsigned long start, unsigned long end)
     /* Calling Expose Page Table System Call */
     ret = expose_page_tbl(pid, &pgtbl_args);
     if (ret < 0) {
-	    free(addr);
-	    munmap(addr1,p4d_size);
-	    munmap(addr2,pud_size);
-	    munmap(addr3,pmd_size);
-	    munmap(addr4,pte_size);
-	    fprintf(stderr, "Error : %s\n", strerror(errno));
-	    printf("Expose Page Table System Call Failed.\n");
-	    /* TODO: return instead of exiting */
-	    exit(EXIT_FAILURE);
+        free(addr);
+        munmap(addr1,p4d_size);
+        munmap(addr2,pud_size);
+        munmap(addr3,pmd_size);
+        munmap(addr4,pte_size);
+        fprintf(stderr, "Error : %s\n", strerror(errno));
+        printf("Expose Page Table System Call Failed.\n");
+        return ret;
     }
     
     /* Page Entries */
     fake_pgd = (unsigned long *)pgtbl_args.fake_pgd;
     
     for (current_va = va_begin; current_va < va_end; current_va += PAGE_SIZE) {
-	    f_pgd = (unsigned long *)fake_pgd[page_index( current_va, pgtbl_info.pgdir_shift )];
+        f_pgd = (unsigned long *)fake_pgd[page_index( current_va, pgtbl_info.pgdir_shift )];
             if(f_pgd == 0)
                     goto verbose_continue;
     
-            /*f_p4d = (unsigned long *)f_pgd[page_index( current_va, pgtbl_info.p4d_shift )];
-             * if (f_p4d == 0)
-             *     goto verbose_continue;
-             *
-             */
+	    if (pgtbl_info.pgdir_shift != pgtbl_info.p4d_shift) {
+            	f_p4d = (unsigned long *)f_pgd[page_index( current_va, pgtbl_info.p4d_shift )];
+            	if (f_p4d == 0)
+                	goto verbose_continue;
 
-            f_pud = (unsigned long *)f_pgd[page_index( current_va, pgtbl_info.pud_shift )]; 
-            if (f_pud == 0)
-                    goto verbose_continue;
+            	f_pud = (unsigned long *)f_p4d[page_index( current_va, pgtbl_info.pud_shift )];
+            	if (f_pud == 0)
+                	goto verbose_continue;
+            } else {
+            	f_pud = (unsigned long *)f_pgd[page_index( current_va, pgtbl_info.pud_shift )];
+            	if (f_pud == 0)
+                	goto verbose_continue;
+      	    }
 
             f_pmd = (unsigned long *)f_pud[page_index( current_va, pgtbl_info.pmd_shift )]; 
             if (f_pmd == 0)
@@ -226,101 +230,192 @@ verbose_continue:
 
 int main(int argc, char *argv[])
 {
-   
+    int ret, read_var;
+
     printf("\n============================================\n");
     printf("TESTCASE 1: Allocating heap memory but not using it.\n");
     printf("============================================\n\n");
     
     printf("virtual_addr   physical_addr Y D W U\n");
     struct temp *test1 = malloc(sizeof(struct temp));
-	if (!test1)
-		return -ENOMEM;
-    print_pgtbl_range((unsigned long) test1, (unsigned long) test1 + sizeof(struct temp) - 1);
+    if (!test1)
+        return -ENOMEM;
+    ret = get_pagetable_range((unsigned long) test1, (unsigned long) test1 + sizeof(struct temp) - 1);
+
+    if (ret < 0) {
+        free(test1);
+        return -1;
+    }
 
     printf("\n============================================\n");
     printf("TESTCASE 2: Write Fault\n");
     printf("============================================\n\n");
     
     struct temp *test2 = malloc(sizeof(struct temp));
-	if (!test2)
-		return -ENOMEM;
+    if (!test2) {
+        free(test1);
+        return -ENOMEM;
+    }
+
     printf("Before Write Fault\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test2, (unsigned long) test2 + sizeof(struct temp) - 1);
+    ret = get_pagetable_range((unsigned long) test2, (unsigned long) test2 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        return -1;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
         test2->a[i] = 5;
         
     printf("\nAfter Write Fault\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test2, (unsigned long) test2 + sizeof(struct temp) - 1);
+    ret = get_pagetable_range((unsigned long) test2, (unsigned long) test2 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        return -1;
+    }
 
     printf("\n============================================\n");
     printf("TESTCASE 3: Read Fault followed by a Write\n");
     printf("============================================\n\n");
     
     struct temp *test3 = malloc(sizeof(struct temp));
-	if (!test3)
-		return -ENOMEM;
-    int temp;
+    if (!test3) {
+        free(test1);
+        free(test2);
+        return -ENOMEM;
+    }
+
     printf("Before Read Fault\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test3,(unsigned long) test3 + sizeof(struct temp) - 1);
-	
+    ret = get_pagetable_range((unsigned long) test3,(unsigned long) test3 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        return -1;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
-        temp = test3->a[i];
+        read_var = test3->a[i];
+
     printf("\nAfter Read Fault\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test3, (unsigned long) test3 + sizeof(struct temp) - 1);
-	
+    ret = get_pagetable_range((unsigned long) test3, (unsigned long) test3 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        return -1;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
         test3->a[i] = 5;
-	printf("\nAfter Write\n");
+    printf("\nAfter Write\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test3, (unsigned long) test3 + sizeof(struct temp) - 1);
-    
+    ret = get_pagetable_range((unsigned long) test3, (unsigned long) test3 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        return -1;
+    }
+
     printf("\n============================================\n");
     printf("TESTCASE 4: Write (without fault)\n");
     printf("============================================\n\n");
     
     struct temp *test4 = malloc(sizeof(struct temp));
-	if (!test4)
-		return -ENOMEM;
+    if (!test4) {
+        free(test1);
+        free(test2);
+        free(test3);
+        return -ENOMEM;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
         test4->a[i] = 5;
     printf("Before Write\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test4, (unsigned long) test4 + sizeof(struct temp) - 1);
+    ret = get_pagetable_range((unsigned long) test4, (unsigned long) test4 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        free(test4);
+        return -1;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
         test4->a[i] = 15;
     printf("\nAfter Write\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test4, (unsigned long) test4 + sizeof(struct temp) - 1);
+    ret = get_pagetable_range((unsigned long) test4, (unsigned long) test4 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        free(test4);
+        return -1;
+    }
 
     printf("\n============================================\n");
     printf("TESTCASE 5: Copy On Write\n");
     printf("============================================\n");
     
     struct temp *test5 = malloc(sizeof(struct temp));
-	if (!test5)
-		return -ENOMEM;
+    if (!test5) {
+        free(test1);
+        free(test2);
+        free(test3);
+        free(test4);
+        return -ENOMEM;
+    }
+
     for (unsigned long i = 0; i < 9999; ++i)
         test5->a[i] = 5;
     printf("\nParent\n");
     printf("virtual_addr   physical_addr Y D W U\n");
-    print_pgtbl_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
+    ret = get_pagetable_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
+    if (ret < 0) {
+        free(test1);
+        free(test2);
+        free(test3);
+        free(test4);
+        free(test5);
+        return -1;
+    }
+
     if (fork() == 0) {
         printf("\nChild\n");
         printf("virtual_addr   physical_addr Y D W U\n");
-        print_pgtbl_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
+        ret = get_pagetable_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
+        if (ret < 0) {
+            free(test1);
+            free(test2);
+            free(test3);
+            free(test4);
+            free(test5);
+            return -1;
+        }
 
-	for (unsigned long i = 0; i < 9999; ++i)
-		test5->a[i] = 5;
+    	for (unsigned long i = 0; i < 9999; ++i)
+        	test5->a[i] = 5;
 
-	printf("\nAfter Child writes\n");
+        printf("\nAfter Child writes\n");
         printf("virtual_addr   physical_addr Y D W U\n");
-        print_pgtbl_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
-
-        //exit(1);
+        ret = get_pagetable_range((unsigned long) test5, (unsigned long) test5 + sizeof(struct temp) - 1);
+        if (ret < 0) {
+            free(test1);
+            free(test2);
+            free(test3);
+            free(test4);
+            free(test5);
+            return -1;
+        }
     }
 
     else
@@ -332,5 +427,6 @@ int main(int argc, char *argv[])
     free(test4);
     free(test5);
 
-    return 0;
+    read_var = 0;
+    return read_var;
 }
